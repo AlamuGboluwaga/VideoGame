8a76e3956d2e8d0143523235f8e9927521c0c85f
Gboluwaga Alamu <alamugboluwaga402@gmail.com>
2026-04-22 11:09:43 +0100

    first commit

---
+++ VideoGAmeApi/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+namespace VideoGAmeApi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Multiply()
+           }
+
+        static void Multiply(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+    }
+
+
+}
