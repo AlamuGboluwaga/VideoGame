79d155966bf9dff63a771ff84dd8d0d6fe35b805
Gboluwaga Alamu <alamugboluwaga402@gmail.com>
2026-05-14 19:37:57 +0100

    videogame input validation

--- VideoGame/Models/Domain/VideoGames.cs
+++ VideoGame/Models/Domain/VideoGames.cs
@@ -1,11 +1,21 @@
-﻿namespace VideoGame.Models.Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoGame.Models.Domain
 {
     public class VideoGames
     {
         public Guid Id { get; set; } = Guid.NewGuid();
+        [Required(ErrorMessage = "UserName is required")]
+        [StringLength(20, MinimumLength = 3)]
         public string Title { get; set; } = string.Empty;
+        [Required(ErrorMessage = "UserName is required")]
+        [StringLength(20, MinimumLength = 3)]
         public string Platform { get; set; } = string.Empty;
+        [Required(ErrorMessage = "UserName is required")]
+        [StringLength(20, MinimumLength = 3)]
         public string Developer { get; set; } = string.Empty;
+        [Required(ErrorMessage = "UserName is required")]
+        [StringLength(20, MinimumLength = 3)]
         public string Publisher { get; set; } = string.Empty;
 
     }
