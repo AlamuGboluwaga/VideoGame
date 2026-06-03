acfd943440dc0224f1921bb9632064fdf6cf365c
Gboluwaga Alamu <alamugboluwaga402@gmail.com>
2026-06-03 12:30:28 +0100

    constuctor updated

--- VideoGame/Controllers/ProdutController.cs
+++ VideoGame/Controllers/ProdutController.cs
@@ -3,23 +3,25 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using VideoGame.Data;
 using VideoGame.Models.DTOs;
+using VideoGame.ProductPagination;
 
 namespace VideoGame.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProdutController(VideoGameDb dbContext) : ControllerBase
+    public class ProductsController(VideoGameDb dbContext) : ControllerBase
     {
         private readonly VideoGameDb _dbContext = dbContext; 
 
         [HttpGet]
-        public async Task<ActionResult<VideoGamesDTO>> GetAllProducts()
+        public async Task<ActionResult<VideoGamesDTO>> GetAllProducts([FromQuery] ProductFilters filter)
 
         {
-            var products = await _dbContext.Products.ToListAsync();
-            var productsCount =await _dbContext.Products.CountAsync()  ;
+            var totalRecords = await _dbContext.Products.CountAsync();
+            var products = await _dbContext.Products.Skip((filter.PageNumber -1)* filter.PageSize).Take(filter.PageSize).ToListAsync();
+            var paginationResponse = new ProductPaginationResponse( filter.PageNumber, filter.PageSize, totalRecords);
 
-            return Ok(new {ProductsCount = productsCount ,Products = products });
+            return Ok( new {Count = totalRecords , PaginationResponse = paginationResponse, Products = products });
         }
     }
 }
--- VideoGame/ProductPagination/ProductFilters .cs
+++ VideoGame/ProductPagination/ProductFilters .cs
@@ -1,8 +1,8 @@
 ﻿namespace VideoGame.ProductPagination
 {
-    public class ProductFilters(int pageNumber =1 , int pageSize =10)
+    public class ProductFilters
     {
-        public int PageNumber { get; set; } = pageNumber < 1 ? 1 :pageNumber ;
-        public int PageSize { get; set; } = pageSize > 100 ? 100 : pageSize;
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
     }
 }
